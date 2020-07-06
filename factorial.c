7963d98059162ed6342be9a8487d6a250065173b
Reetpriye <reetpriye@gmail.com>
2020-07-06 20:10:51 +0530

    Code Refactor

--- factorial.c
+++ factorial.c
@@ -1,24 +1,19 @@
+// Factorial using recursion
+// Author: REET
+
 #include <stdio.h>
-#include <limits.h>
-#include <float.h>
-long double fact(long double n) {
-if ( n == 1 || n ==0 )
-return 1;
-else
+
+long double fact(long double n)
 {
-    return n * fact(n-1);
+    if (n == 1 || n == 0) return 1;
+    else return n * fact(n - 1);
 }
-
-}
-
 int main()
 {
-    long double num,sum;
+    long double num, sum;
     printf("Enter the number you want factorial of:\n");
-    scanf("%Lf",&num);
+    scanf("%Lf", &num);
     sum = fact(num);
-    printf("Factorial of %.0Lf is %.0Lf\n",num ,sum);
-    printf("Size of long int is : %ld\n",sizeof(long double));
-    printf("Range of Signed Long int is from 0 to %f\n",DBL_MAX);
+    printf("Factorial of %.0Lf is %.0Lf\n", num, sum);
     return 0;
 }
--- palindrome.c
+++ palindrome.c
@@ -1,22 +1,24 @@
-// Program to check whether a no. is palindrome or not.
+// Palindrome Check using While loop
+// Author: REET
 
 #include <stdio.h>
 
-	int main() {
-	
-	long int num,q,revnum=0,rem;
-	
+int main()
+{
+	long int num, q, revnum = 0, rem;
 	printf("Enter the No. to check whether it's palindrome or not?\n");
-	scanf("%ld",&num);
+	scanf("%ld", &num);
 	q = num;
-	
-	while(q != 0) {
-	rem = q%10;
-	revnum = revnum * 10 + rem;
-	q = q/10;
+	while (q != 0)
+	{
+		rem = q % 10;
+		revnum = revnum * 10 + rem;
+		q = q / 10;
 	}
-	if(revnum == num) {
-	printf("It is a palindrome No.\n");
+	if (revnum == num)
+	{
+		printf("It is a palindrome No.\n");
 	}
-	else printf("No! It's not a palindrome No.\n");
-	}
+	else
+		printf("No! It's not a palindrome No.\n");
+}
